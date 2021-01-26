223e3f6b0e695ac43e14f5e7a57aae9ce7b2b80b
ruboon-dej <ruboon_d@yahoo.com>
2021-01-26 17:03:38 +0700

    rename files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="baseconverter",
-    packages=find_packages(include=["lib"]),
+    packages=find_packages(include=["baseconverter"]),
     version="0.1.0",
     author="Ruboon Dej-Udom",
     license="MIT",
